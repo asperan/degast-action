3a48da5c325dd570314d566d4358627e1d400eff
Alex Speranza <alex.speranza@studio.unibo.it>
2021-12-29 11:51:34 +0100

    style(docker): change the changelog format to markdown

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-changelog="$(/app/degast changelog)"
+changelog="$(/app/degast changelog -m)"
 changelog="${changelog//'%'/'%25'}"
 changelog="${changelog//$'\n'/'%0A'}"
 changelog="${changelog//$'\r'/'%0D'}"
