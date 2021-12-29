9b8fb95a170291fb2001f553da17ddfb26802eea
Alex Speranza <alex.speranza@studio.unibo.it>
2021-12-29 11:14:24 +0100

    fix(docker): fix degast executable reference

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "::set-output name=nextTag::$(dgst tag -n)"
-echo "::set-output name=changelog::$(dgst changelog)"
+echo "::set-output name=nextTag::$(/app/degast tag -n)"
+echo "::set-output name=changelog::$(/app/degast changelog)"
